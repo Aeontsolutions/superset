9fa54debb4a053ac33ee8910224705694788b981
Romaine Murray <romaine.murray@aeontsolutions.com>
2024-03-31 02:38:17 -0500

    Fix byteCharacters iteration in convertBase64ToFile function

--- superset-frontend/src/components/GenerateReportModal/index.tsx
+++ superset-frontend/src/components/GenerateReportModal/index.tsx
@@ -73,7 +73,7 @@ export default function GenerateReportModal({
   const convertBase64ToFile = (base64: string, fileName: string) => {
     const byteCharacters = atob(base64);
     const byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++) {
+    for (let i = 0; i < byteCharacters.length; i += 1) {
       byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
     const byteArray = new Uint8Array(byteNumbers);
